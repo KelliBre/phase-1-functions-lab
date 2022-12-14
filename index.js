0f17cfcd658e3e4d6ea1e07fda470983bd526782
KelliBre <Kelli.Brecunier@gmail.com>
2022-12-14 12:51:50 +1000

    commit tree 25f99716bb07421df031fca9b4f83560225efe21

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+const headQuarters = 42;
+function distanceFromHqInBlocks (address){
+    if(address > 42){
+        return address - headQuarters
+    }else{
+        return headQuarters - address
+    }
+}
+
+
+function distanceFromHqInFeet(address){
+    return distanceFromHqInBlocks(address) * 264;
+
+};
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    if(start > destination){
+        const upTown = start - destination;
+        return upTown * 264
+    }else{
+        const downTown = destination - start;
+        return downTown * 264
+    }
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distance  = distanceTravelledInFeet(start, destination);
+    if( distance <= 400){
+        return 0
+    } else if (distance > 400 && distance < 2000){
+        return (distance - 400) * .02;
+    }else if(distance> 2000 && distance < 2500){
+        return 25
+    }else if (distance >= 2500){
+        return 'cannot travel that far'
+    }
+
+  }
